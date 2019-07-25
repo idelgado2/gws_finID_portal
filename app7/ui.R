00944bfe274906c3905779e5a52d29b6df021e62
Isaac Delgado <isaacdelgado@isaacs-mbp.mbayaq.org>
2019-07-25 10:05:54 -0700

    Added UI navbar structure

--- app7/ui.R
+++ app7/ui.R
@@ -4,20 +4,20 @@ library(shiny)
 library(rsconnect)
 
 shinyUI(
-  fluidPage(
-    titlePanel(title = "Hello this is going to be the GroundUp shiny app"),
-    sidebarLayout(
-      sidebarPanel(h3("Enter personal information"),
-                   textInput("name", h4("Enter Your Name"), ""),
-                   textInput("age", h4("Enter Your Age"), ""),
-                   radioButtons("gender", "Select your gender",list("Male", "Female") , ""),
-                   sliderInput("slide", "Select the value from the slider", min = 0, max = 5, value = 2, step = 0.2, animate = TRUE)
-      ),
-      mainPanel(h3("Personal Information"),
-                textOutput("myName"),
-                textOutput("myAge"),
-                textOutput("myGender"),
-                textOutput("mySliderValue"))
-    )
+  navbarPage(
+    id = "form",
+    title = "FinID Data Entry",
+    tabPanel("Survey Info",
+              sidebarPanel(),
+              mainPanel()
+    ),
+    tabPanel("Fin Photo Entry",
+              sidebarPanel(),
+              mainPanel()
+    ),
+    tabPanel("Data Submission",
+              sidebarPanel(),
+              mainPanel()
+    ),
   )
 )
