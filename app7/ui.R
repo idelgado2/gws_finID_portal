2e2efe588933fa02ee98d57b4732b5d565cdbd2d
Isaac Delgado <isaacdelgado@isaacs-mbp.mbayaq.org>
2019-07-25 10:31:27 -0700

    filled in parameters

--- app7/ui.R
+++ app7/ui.R
@@ -7,14 +7,67 @@ shinyUI(
   navbarPage(
     id = "form",
     title = "FinID Data Entry",
+    ###############
+    ##SURVEY INFO##
+    ###############
     tabPanel("Survey Info",
-              sidebarPanel(),
-              mainPanel()
+              sidebarPanel(
+                selectInput(
+                  "site.phid",
+                  labelMandatory("Survey site"),
+                  choices = flds$sites
+                ),
+                dataInput(
+                  "date.phid",
+                  labelMandatory("Survey date"),
+                  value = Sys.Date(),
+                  format = "yyyy-mm-dd"
+                ),
+                hr(),
+                checkboxGroupInput(
+                  "crew",
+                  "Crew",
+                  choices = flds$observers,
+                  inline = T
+                ),
+                textInput(
+                  "vessel",
+                  "Vessel",
+                  placeholder = "MBA Skiff? Norcal? R/V BS?"
+                )
+              ),
+              mainPanel(
+                sliderInput(
+                  "effort",
+                  "Start/Stop of Survey Effort?",
+                  min = 6,
+                  max = 20,
+                  value = c(8, 15)
+                ),
+                textInput(
+                  "notes.survey",
+                  "Notes from survey day",
+                  placeholder = "breaches? predations?",
+                  width = "100%"
+                ),
+                textOutput("crew"),
+                actionButton(
+                  "addfins",
+                  "Ready to add Fins?",
+                  class="btn-primary"
+                )
+              )
     ),
+    ################
+    ##FIN ID ENTRY##
+    ################
     tabPanel("Fin Photo Entry",
               sidebarPanel(),
               mainPanel()
     ),
+    ###################
+    ##DATA SUBMISSION##
+    ###################
     tabPanel("Data Submission",
               sidebarPanel(),
               mainPanel()
