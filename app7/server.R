76a2fcc3ddae5573a511f4d3f18144f97ad01bd1
Isaac Delgado <isaacdelgado@isaacs-mbp.mbayaq.org>
2019-07-25 11:35:40 -0700

    added server side output variables

--- app7/server.R
+++ app7/server.R
@@ -7,10 +7,31 @@ shinyServer(
     setAccountInfo(name='idelgado',
                    token='B911B9733B6FCF8B67DCA5BF861A1AD9',
                    secret='itTB3/LHML67EOMxXkJkrqOLhD+L7w58wvaoqXvW')
-    output$myName = renderText(input$name)
-    output$myAge = renderText(input$age)
-    output$myGender = renderText(input$gender)
-    
-    output$mySliderValue = renderText(paste("You selected value: ", input$slide))
+    output$site.phid = renderText({
+                          paste(
+                            HTML("<font color=\"#FF0000\"><b>SURVEY SITE<font color=\"#000000\"></b> assigned as: "),
+                            HTML(
+                              paste0(
+                                "<font color=\"#FF0000\"><b>",
+                                input$site.phid,
+                                "<b>"
+                              )
+                            )
+                          )
+                        }
+                      )
+    output$date.phid = renderText({
+                          paste(
+                            HTML("<font color=\"#FF0000\"><b>SURVEY DATE<font color=\"#000000\"></b> assigned as: "), 
+                            HTML(
+                              paste0(
+                                "<font color=\"#FF0000\"><b>",
+                                as.Date(input$date.phid, format = "%m-%d-%Y"),
+                                "</b>"
+                              )
+                            )
+                          )
+                        }
+                      )
   }
 )
