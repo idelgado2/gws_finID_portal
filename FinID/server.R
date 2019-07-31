2229261e1838b456c9025029a5a32eefbc258011
Isaac Delgado <isaacdelgado@isaacs-mbp.mbayaq.org>
2019-07-31 12:18:28 -0700

    commenting out shinyapp.io tokens, we wount be needing them anymore since we are publishing it to the aws server

--- FinID/server.R
+++ FinID/server.R
@@ -5,9 +5,9 @@ library(rsconnect)
 source("fin_shiny_fxns2.R")
 shinyServer(
   function(input, output, session){
-    setAccountInfo(name='idelgado',
-                   token='B911B9733B6FCF8B67DCA5BF861A1AD9',
-                   secret='itTB3/LHML67EOMxXkJkrqOLhD+L7w58wvaoqXvW')
+    #setAccountInfo(name='idelgado',
+    #               token='B911B9733B6FCF8B67DCA5BF861A1AD9',
+    #               secret='itTB3/LHML67EOMxXkJkrqOLhD+L7w58wvaoqXvW')
     
     output$siteOutput.phid = renderUI(tags$p(tags$span(style="color:red", "SURVEY SITE"), "assigned as: ", tags$span(style="color:red", input$site.phid)))
     output$dateOutput.phid = renderUI(tags$p(tags$span(style="color:red", "SURVEY DATE"), "assigned as: ", tags$span(style="color:red", as.Date(input$date.phid, format = "%m-%d-%Y"))))
